e8521e136590a6138f97fe1cac3deea21b5d543c
SebasGonza <sebasgoro41@gmail.com>
2022-10-12 07:08:50 -0500

    adicion de formulario login

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { User } from 'src/app/core/models/usuariosDto';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent{
 
-  constructor() { }
+  user: User = {
+    username: '',
+    document: '',
+    email: '',
+    documentType: 0,
+    password: '',
+    phoneNumber:''
 
-  ngOnInit(): void {
+  };
+
+  error:string = '';
+  respuestaError:Boolean = false;
+
+  miFormulario: FormGroup = this.fmbuilder.group({
+    email: ['', Validators.required],
+    password: ['', [Validators.required]]
+  });
+
+
+
+  constructor(private fmbuilder: FormBuilder ) { }
+
+  login(){
+    this.user.email = this.miFormulario.controls['correo'].value;
+    this.user.email = this.miFormulario.controls['password'].value;
   }
 
 }
