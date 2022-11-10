6e54df38d85d54410d45c598581a301aafd94544
SebasGonza <sebasgoro41@gmail.com>
2022-11-10 06:12:02 -0500

    add sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/auth/pages/login/login.component';
 
-const routes: Routes = [{ path: 'home', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/sidebar/navbar-data.ts
@@ -0,0 +1,7 @@
+export const navbarData = [
+  {
+    routerLink: 'usuario',
+    icon: 'fa-solid fa-user',
+    label: 'Usuarios'
+  },
+];
